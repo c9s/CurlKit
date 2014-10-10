3985e076c9fb7082f12b76d61a684236e5ee4cd1
c9s <yoanlin93@gmail.com>
2014-10-10 19:35:43 +0800

    Add tests

---
+++ tests/CurlKit/CurlAgentTest.php
@@ -0,0 +1,36 @@
+<?php
+
+class CurlAgentTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @expectedException CurlKit\CurlException
+     */
+    public function testError() {
+        $agent = new CurlKit\CurlAgent;
+        ok($agent);
+        $response = $agent->get('http://does.not.exist');
+    }
+
+    public function testGet()
+    {
+        $agent = new CurlKit\CurlAgent;
+        ok($agent);
+
+        $response = $agent->get('https://stackoverflow.com/questions/11297320/using-a-try-catch-with-curl-in-php');
+        ok($response);
+        ok($response->body);
+        ok($response->headers);
+        ok(is_array($response->headers));
+    }
+
+    public function testingProxy() {
+        $agent = new CurlKit\CurlAgent;
+        $agent->setProxy('106.187.96.49:3128');
+        $response = $agent->get('https://stackoverflow.com/questions/11297320/using-a-try-catch-with-curl-in-php');
+        ok($response);
+        ok($response->body);
+        ok($response->headers);
+    }
+}
+
